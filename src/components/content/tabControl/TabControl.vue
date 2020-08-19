89f9d79cc87fe495094bba85fc549e17ac22ab93
顾念念 <2293560362@qq.com>
2020-08-19 12:02:08 +0800

    master

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tab-control">
+    <div
+      class="tab-control-item"
+      v-for="(item,index) in titles"
+      :key="item"
+      :class="{active:index === currentIndex}"
+    >{{item}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      currentIndex: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  height: 40px;
+  line-height: 40px;
+}
+.tab-control-item {
+  flex: 1;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,42 +8,163 @@
     <home-swiper :banners="banners" />
     <!-- recommend -->
     <recommend-view :recommends="recommends" />
+    <!-- feature -->
+    <feature-view />
+    <!--  -->
+    <tab-control :titles="titles" />
+    <ul>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+      <li>liebiao</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-import { getHomeMultidata } from 'network/home'
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+
+import { getHomeMultidata } from 'network/home'
 
 export default {
   name: 'Home',
   data () {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      titles: ['流行', '新款', '精选']
     }
   },
   components: {
-    NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
+
+    NavBar,
+    TabControl
   },
   created () {
     // 请求多个数据
     getHomeMultidata().then(res => {
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
-      console.log(this.banners);
     })
   }
 }
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
 }
 </style>
